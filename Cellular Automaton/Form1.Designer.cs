4fff67cc536a1147c7af4b9c83a624b3e61208d3
pzf0000 <pzf0000@foxmail.com>
2018-04-16 00:52:25 +0800

    init

---
+++ Cellular Automaton/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Cellular_Automaton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_length = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_height = new System.Windows.Forms.TextBox();
+            this.button_set = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_x = new System.Windows.Forms.TextBox();
+            this.textBox_y = new System.Windows.Forms.TextBox();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_start = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.button_resetup = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "长：";
+            // 
+            // textBox_length
+            // 
+            this.textBox_length.Location = new System.Drawing.Point(47, 11);
+            this.textBox_length.Name = "textBox_length";
+            this.textBox_length.Size = new System.Drawing.Size(100, 21);
+            this.textBox_length.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "宽：";
+            // 
+            // textBox_height
+            // 
+            this.textBox_height.Location = new System.Drawing.Point(188, 11);
+            this.textBox_height.Name = "textBox_height";
+            this.textBox_height.Size = new System.Drawing.Size(100, 21);
+            this.textBox_height.TabIndex = 3;
+            // 
+            // button_set
+            // 
+            this.button_set.Location = new System.Drawing.Point(295, 10);
+            this.button_set.Name = "button_set";
+            this.button_set.Size = new System.Drawing.Size(75, 23);
+            this.button_set.TabIndex = 4;
+            this.button_set.Text = "设置";
+            this.button_set.UseVisualStyleBackColor = true;
+            this.button_set.Click += new System.EventHandler(this.Button_set_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(376, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "x";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(469, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "y";
+            // 
+            // textBox_x
+            // 
+            this.textBox_x.Location = new System.Drawing.Point(393, 11);
+            this.textBox_x.Name = "textBox_x";
+            this.textBox_x.Size = new System.Drawing.Size(70, 21);
+            this.textBox_x.TabIndex = 7;
+            // 
+            // textBox_y
+            // 
+            this.textBox_y.Location = new System.Drawing.Point(486, 11);
+            this.textBox_y.Name = "textBox_y";
+            this.textBox_y.Size = new System.Drawing.Size(70, 21);
+            this.textBox_y.TabIndex = 8;
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(562, 10);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(75, 23);
+            this.button_add.TabIndex = 9;
+            this.button_add.Text = "添加";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(643, 10);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(75, 23);
+            this.button_start.TabIndex = 10;
+            this.button_start.Text = "开始";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(868, 585);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // button_stop
+            // 
+            this.button_stop.Location = new System.Drawing.Point(724, 10);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(75, 23);
+            this.button_stop.TabIndex = 12;
+            this.button_stop.Text = "停止";
+            this.button_stop.UseVisualStyleBackColor = true;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
+            // 
+            // button_resetup
+            // 
+            this.button_resetup.Location = new System.Drawing.Point(805, 11);
+            this.button_resetup.Name = "button_resetup";
+            this.button_resetup.Size = new System.Drawing.Size(75, 23);
+            this.button_resetup.TabIndex = 13;
+            this.button_resetup.Text = "重置";
+            this.button_resetup.UseVisualStyleBackColor = true;
+            this.button_resetup.Click += new System.EventHandler(this.button_resetup_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 636);
+            this.Controls.Add(this.button_resetup);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.textBox_y);
+            this.Controls.Add(this.textBox_x);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button_set);
+            this.Controls.Add(this.textBox_height);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_length);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cellular Automaton";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_length;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_height;
+        private System.Windows.Forms.Button button_set;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_x;
+        private System.Windows.Forms.TextBox textBox_y;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_stop;
+        private System.Windows.Forms.Button button_resetup;
+    }
+}
+
---
+++ Cellular Automaton/Form1.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Cellular_Automaton
+{
+    public partial class Form1 : Form
+    {
+        private int length = 0;
+        private int height = 0;
+        private double u = 10;
+        private Graphics g;
+        private int[,] cell1;
+        private int[,] cell2;
+        private int time = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox_x.Enabled = false;
+            textBox_y.Enabled = false;
+            button_add.Enabled = false;
+            button_start.Enabled = false;
+            g = pictureBox1.CreateGraphics();
+            t = new Thread(new ThreadStart(ThreadProc));
+            t.IsBackground = true;
+            pictureBox1.Enabled = false;
+            button_stop.Enabled = false;
+        }
+
+        private void Button_set_Click(object sender, EventArgs e)
+        {
+            if(textBox_length.Text != null && textBox_height.Text != null)
+            {
+                if(int.TryParse(textBox_length.Text, out length) == true && int.TryParse(textBox_height.Text, out height) == true
+                    && length <= Size.Width / u && height <= Size.Height / u)
+                {
+                    textBox_length.Enabled = false;
+                    textBox_height.Enabled = false;
+                    textBox_x.Enabled = true;
+                    textBox_y.Enabled = true;
+                    button_add.Enabled = true;
+                    button_start.Enabled = true;
+                    cell1 = new int[length, height];
+                    cell2 = new int[length, height];
+                    pictureBox1.Enabled = true;
+                    button_set.Enabled = false;
+                    for(int i = 0; i < length; i++)
+                    {
+                        for(int j = 0; j < height; j++)
+                        {
+                            repaint(i, j);
+                        }
+                    }
+                }
+                else
+                {
+                    textBox_length.Text = "";
+                    textBox_height.Text = "";
+                }
+            }
+            else
+            {
+                textBox_length.Text = "";
+                textBox_height.Text = "";
+            }
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            int x = 0;
+            int y = 0;
+            if (textBox_x.Text != null && textBox_y.Text != null)
+            {
+                if (int.TryParse(textBox_x.Text, out x) == true && int.TryParse(textBox_y.Text, out y) == true)
+                {
+                    if(x <= 0 || y <= 0 || x > length || y > length)
+                    {
+                        textBox_x.Text = "";
+                        textBox_y.Text = "";
+                        return;
+                    }
+                    cell1[x-1, y-1] = 1;
+                    paint(x-1, y-1);
+                }
+            }
+        }
+        public static Thread t;
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            textBox_x.Enabled = false;
+            textBox_y.Enabled = false;
+            button_add.Enabled = false;
+            
+            try
+            {
+                t.Start();
+                Console.WriteLine(t.ThreadState.ToString());
+            }
+            catch
+            {
+#pragma warning disable CS0618 // 类型或成员已过时
+                t.Resume();
+#pragma warning restore CS0618 // 类型或成员已过时
+                Console.WriteLine(t.ThreadState.ToString());
+            }
+            button_stop.Enabled = true;
+        }
+
+        private void ThreadProc()
+        {
+            while (true)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    for (int j = 0; j < height; j++)
+                    {
+                        if (time == 1)
+                        {
+                            int count = 0;
+                            if (i + 1 < length && j + 1 < height)
+                            {
+                                if (cell1[i + 1, j + 1] == 1)
+                                {
+                                    count++;
+                                }
+                            }
+                            if (i + 1 < length)
+                            {
+                                if (cell1[i + 1, j] == 1)
+                                {
+                                    count++;
+                                }
+                            }
+                            if (i + 1 < length && j - 1 > 0)
+                            {
+                                if (cell1[i + 1, j - 1] == 1)
+                                {
+                                    count++;
+                                }
+                            }
+                            if (j - 1 > 0)
+                            {
+                                if (cell1[i, j - 1] == 1)
+                                {
+                                    count++;
+                                }
+                            }
+                            if (i - 1 > 0 && j - 1 > 0)
+                            {
+                                if (cell1[i - 1, j - 1] == 1)
+                                {
+                                    count++;
+                                }
+                            }
+                            if (i - 1 > 0)
+                            {
+                                if (cell1[i - 1, j] == 1)
+                                {
+                                    count++;
+                                }
+                            }
+                            if (i - 1 > 0 && j + 1 < height)
+                            {
+                                if (cell1[i - 1, j + 1] == 1)
+                                {
+                                    count++;
+                                }
+                            }
+                            if (j + 1 < height)
+                            {
+                                if (cell1[i, j + 1] == 1)
+                                {
+                                    count++;
+                                }
+                            }
+                            if (cell1[i, j] == 1)
+                            {
+                                if (count == 2 || count == 3)
+                                {
+                                    cell2[i, j] = 1;
+                                }
+                                else
+                                {
+                                    cell2[i, j] = 0;
+                                }
+                            }
+                            else
+                            {
+                                if (count == 3)
+                                {
+                                    cell2[i, j] = 1;
+                                }
+                                else
+                                {
+                                    cell2[i, j] = 0;
+                                }
+                            }
+                        }
+                        if (time == 2)
+                        {
+                            int count = 0;
+                            if (i + 1 < length && j + 1 < height)
+                            {
+                                if (cell2[i + 1, j + 1] == 1)
+                                {
+                                    count++;
+                                }
+                            }
+                            if (i + 1 < length)
+                            {
+                                if (cell2[i + 1, j] == 1)
+                                {
+                                    count++;
+                                }
+                            }
+                            if (i + 1 < length && j - 1 > 0)
+                            {
+                                if (cell2[i + 1, j - 1] == 1)
+                                {
+                                    count++;
+                                }
+                            }
+                            if (j - 1 > 0)
+                            {
+                                if (cell2[i, j - 1] == 1)
+                                {
+                                    count++;
+                                }
+                            }
+                            if (i - 1 > 0 && j - 1 > 0)
+                            {
+                                if (cell2[i - 1, j - 1] == 1)
+                                {
+                                    count++;
+                                }
+                            }
+                            if (i - 1 > 0)
+                            {
+                                if (cell2[i - 1, j] == 1)
+                                {
+                                    count++;
+                                }
+                            }
+                            if (i - 1 > 0 && j + 1 < height)
+                            {
+                                if (cell2[i - 1, j + 1] == 1)
+                                {
+                                    count++;
+                                }
+                            }
+                            if (j + 1 < height)
+                            {
+                                if (cell2[i, j + 1] == 1)
+                                {
+                                    count++;
+                                }
+                            }
+                            if (cell2[i, j] == 1)
+                            {
+                                if (count == 2 || count == 3)
+                                {
+                                    cell1[i, j] = 1;
+                                }
+                                else
+                                {
+                                    cell1[i, j] = 0;
+                                }
+                            }
+                            else
+                            {
+                                if (count == 3)
+                                {
+                                    cell1[i, j] = 1;
+                                }
+                                else
+                                {
+                                    cell1[i, j] = 0;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (time == 1)
+                {
+                    paint1();
+                    time = 2;
+                }
+                else
+                {
+                    paint2();
+                    time = 1;
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void paint2()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (cell2[i, j] == 1)
+                    {
+                        paint(i, j);
+                    }
+                    else
+                    {
+                        repaint(i, j);
+                    }
+                }
+            }
+        }
+
+        private void paint1()
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (cell1[i, j] == 1)
+                    {
+                        paint(i, j);
+                    }
+                    else
+                    {
+                        repaint(i, j);
+                    }
+                }
+            }
+        }
+
+        private void paint(int x, int y)
+        {
+            Rectangle rect = new Rectangle((int)(u * x), (int)(u * y), (int)u, (int)u);
+            SolidBrush b1 = new SolidBrush(Color.Red);       
+            g.FillRectangle(b1, rect);
+        }
+        private void repaint(int x, int y)
+        {
+            Rectangle rect = new Rectangle((int)(u * x), (int)(u * y), (int)u, (int)u);
+            SolidBrush b1 = new SolidBrush(Color.White);
+            g.FillRectangle(b1, rect);
+        }
+
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+#pragma warning disable CS0618 // 类型或成员已过时
+            t.Suspend();
+#pragma warning restore CS0618 // 类型或成员已过时
+            textBox_x.Enabled = true;
+            textBox_y.Enabled = true;
+            button_add.Enabled = true;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+            x = x / (int)u;
+            y = y / (int)u;
+            if(x >= length || y >= height)
+            {
+                return;
+            }
+            if (time == 1)
+            {
+                cell1[x, y] = 1;
+                paint(x, y);
+            }
+            else
+            {
+                cell2[x, y] = 1;
+                paint(x, y);
+            }
+        }
+
+        private void button_resetup_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+            Application.Exit();
+            Application.Restart();
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
